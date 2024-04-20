65f955581b1d1fd9a20dbd534bb11b7f04e4af93
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-04-20 01:26:09 +0100

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package {'flask':
+# Install an especific version of flask (2.1.0) from pip
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  name     => 'Werkzeug',
+}
+package { 'Flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+  name     => 'flask',
 }
