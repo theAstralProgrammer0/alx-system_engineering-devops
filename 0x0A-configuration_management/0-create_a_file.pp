c74ecf1135c165f467651f35a98a44f7d6c01a0d
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-04-20 01:00:39 +0100

    task 0 debug

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # This manifest creates a file at /tmp
 file { '/tmp/school':
     ensure  => 'file',
+    mode    => '0744',
     owner   => 'www-data',
     group   => 'www-data',
-    mode    => '0744',
-    content => 'I love Puppet',
+    content => 'I love Puppet'
 }
