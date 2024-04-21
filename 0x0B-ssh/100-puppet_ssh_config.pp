be5bf807b5fbaa7406eb1733ca7c9c372594e85f
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-04-21 05:34:18 +0100

    task 100

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,22 @@
+# This is a puppet manifest that automates ssh configuration
+$sshd = "~/.ssh"
+$sshcf = "~/.ssh/config"
+
+file { $sshd:
+	ensure => directory,
+	mode => '0700',
+	owner => $::environment['USER'],
+	group => $::environment['USER'],
+}
+
+file { $sshcf:
+	ensure => file,
+	mode => '0600',
+	owner => $::environment['USER'],
+	group => $::environment['USER'],
+	content => "
+		PasswordAuthentication no
+		IdentityFile ~/.ssh/school
+		",
+}
+
