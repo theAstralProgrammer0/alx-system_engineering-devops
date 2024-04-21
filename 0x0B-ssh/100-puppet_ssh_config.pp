e5cbb90bb4f38148ce3a2a6521d9cf8e724bdf98
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-04-21 05:42:17 +0100

    task 100 debug

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,19 +1,15 @@
 # This is a puppet manifest that automates ssh configuration
-$sshd = "~/.ssh"
-$sshcf = "~/.ssh/config"
+$sshd = "/home/ubuntu/.ssh/"
+$sshcf = "/home/ubuntu/.ssh/config"
 
 file { $sshd:
 	ensure => directory,
 	mode => '0700',
-	owner => $::environment['USER'],
-	group => $::environment['USER'],
 }
 
 file { $sshcf:
 	ensure => file,
 	mode => '0600',
-	owner => $::environment['USER'],
-	group => $::environment['USER'],
 	content => "
 		Host *
 			PasswordAuthentication no
