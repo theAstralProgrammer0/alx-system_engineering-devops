8d93b4cddc78da977c7858e79caee1c695244339
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-04-25 05:40:11 +0100

    task 7

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,30 +1,30 @@
 # This puppet manifest automates nginx web server installation, adds redirection, and handles 404 errors
 
 package {'nginx':
-	ensure => 'present',
+  ensure => 'present',
 }
 
 exec {'Install Nginx':
-	command => 'apt-get update; apt-get -y install nginx',
-	provider => shell,
+  command => 'sudo apt-get update; sudo apt-get -y install nginx',
+  provider => shell,
 }
 
 exec {'Allow Firewall':
-	command => 'ufw allow "Nginx HTTP"',
-	provider => ubuntu,
+  command => 'sudo ufw allow "Nginx HTTP"',
+  provider => ubuntu,
 }
 
 exec {'Populate index.html File(s)':
-	command => 'echo "Hello World!" > /var/www/html/index.html',
-	provider => shell,
+  command => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
+  provider => shell,
 }
 
 exec {'301 Moved Permanently':
-	command => 'sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4 permanent;/" /etc/nginx/sites-enabled/default',
-	provider => shell,
+  command => 'sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4 permanent;/" /etc/nginx/sites-enabled/default',
+  provider => shell,
 }
 
 exec {'Restart Nginx':
-	command => 'service nginx restart',
-	provider => shell,
+  command => 'sudo service nginx restart',
+  provider => shell,
 }
