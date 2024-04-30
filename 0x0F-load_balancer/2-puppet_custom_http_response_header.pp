c914cc16aabc0cbed530aca2882b778f4a0f3492
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-04-30 05:32:06 +0100

    task 02 debugging

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,20 +1,11 @@
 # This puppet manifest automates creating a custom HTTP header response for web servers
 
-file_line { 'add_custom_header':
-  path    => '/etc/nginx/nginx.conf',
-  line    => '\n\tadd_header X-Served-By $hostname;',
-  match   => '^http {',
-  ensure  => present,
-  require => Package['nginx'],
-  notify  => Service['nginx'],
-}
-
-service { 'nginx':
-  ensure => running,
-  enable => true,
+exec { 'add_custom_header':
+  command  => "sed -i 's/server_name _;/server_name _;\\n\\tadd_header X-Served-By $hostname;/' /etc/nginx/sites-enabled/default",
+  provider => 'shell',
 }
 
 exec { 'restart':
-  command: 'service nginx restart',
-  provider: shell
+  command => 'service nginx restart',
+  provider => 'shell',
 }
