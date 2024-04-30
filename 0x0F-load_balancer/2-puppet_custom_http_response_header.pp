bf6f0fc1c6d3f0782640175dcc2c2f91e59943f2
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-04-30 04:56:10 +0100

    task 02 debug

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -2,8 +2,8 @@
 
 file_line {'add_header':
   ensure => present,
-  path => /etc/nginx/sites-enabled/default,
+  path => '/etc/nginx/sites-enabled/default',
   line => '\n\tadd_header X-Served-By $hostname;',
-  match => '^server_name _;'
-  after => '^server_name _;'
+  match => '^server_name _;',
+  after => '^server_name _;',
 }
