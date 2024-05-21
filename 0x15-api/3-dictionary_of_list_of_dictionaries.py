78efb06e938ecaf2f96ffb9b9374a94be5dd5721
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-05-21 06:01:34 +0100

    task 3

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""This module accesses a REST API to fetch employee todo details"""
+import json
+import requests
+import sys
+
+
+def get_emp_todo_progress(emp_id):
+    """This function gets and filters employee details"""
+    # Fetch employee details
+    users_url = f"https://jsonplaceholder.typicode.com/users"
+    users_response = requests.get(users_url)
+    if users_response.status_code != 200:
+        print(f"Users not found.")
+        return
+    users = users_response.json()
+
+    all_tasks = {}
+
+    for emp in users:
+        emp_id = emp['id']
+        # Fetch todos for the employee
+        todos_url = f"https://jsonplaceholder.typicode.com/todos?userId={emp_id}"
+        todos_response = requests.get(todos_url)
+        todos = todos_response.json()
+
+        # Prepare json data
+        tasks = [
+                    {
+                        "username": emp['username'],
+                        "task": todo['title'],
+                        "completed": todo['completed']
+                    }
+                    for todo in todos
+                ]
+                    
+        all_tasks[emp_id] = tasks
+
+    # Save json data into json file
+    json_file = f"todo_all_employees.json"
+    with open(json_file, mode='w', encoding='utf-8', newline='') as f:
+        json.dump(all_tasks, f)
+
+    # print(f"Data exported to {emp_id}.json file")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <emp_id>")
+        sys.exit(1)
+
+    try:
+        emp_id = int(sys.argv[1])
+        get_emp_todo_progress(emp_id)
+    except ValueError:
+        print("emp ID must be an integer.")
+        sys.exit(1)
