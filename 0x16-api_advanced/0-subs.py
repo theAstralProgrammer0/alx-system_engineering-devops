17cf0b7fa362ac985b4ddf933f55912a800fb925
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-06-05 04:43:56 +0100

    task 0 pcs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
+"""Module for retrieving number of subscribers from a subreddit"""
 import requests
 
+
 def number_of_subscribers(subreddit):
+    """fetch and return number of subscribers"""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'My User Agent 1.0'}
+
+    headers = {'User-Agent': 'curl/7.68.0'}
+
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
