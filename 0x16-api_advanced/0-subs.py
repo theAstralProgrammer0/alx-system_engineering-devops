06dcaff588d249732947f1e164cfbaf58f38e271
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-06-05 05:14:05 +0100

    task 0 deubber

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,17 +8,14 @@ def number_of_subscribers(subreddit):
     if subreddit is None or not isinstance(subreddit, str):
         return 0
 
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
 
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0;\
-Win64;\
- x64) AppleWebKit/537.36 (KHTML, like Gecko) \
- Chrome/125.0.0.0 Safari/537.36'}
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows N'}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
-        data = response.json()
-        return data['data']['subscribers']
+        data = response.json().get('data')
+        return data.get('subscribers')
     else:
         return 0
