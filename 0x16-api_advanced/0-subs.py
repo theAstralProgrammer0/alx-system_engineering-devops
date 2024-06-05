1f61c96bac643167f84739fdd33ca9cd324bc521
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-06-05 05:06:31 +0100

    task 0 debugging

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,9 +5,15 @@ import requests
 
 def number_of_subscribers(subreddit):
     """fetch and return number of subscribers"""
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
 
-    headers = {'User-Agent': 'My User Agent 1.0'}
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0;\
+Win64;\
+ x64) AppleWebKit/537.36 (KHTML, like Gecko) \
+ Chrome/125.0.0.0 Safari/537.36'}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
 
