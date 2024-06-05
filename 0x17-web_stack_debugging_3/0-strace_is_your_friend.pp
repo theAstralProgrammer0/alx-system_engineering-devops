44d3d34a2dd37d22d6580df22d97deff9be44670
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-06-05 18:07:13 +0100

    new puppet config

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,9 +1,20 @@
-# Ensure the puppetlabs-stdlib module is installed
-# This can be installed via the Puppet Forge: puppet module install puppetlabs-stdlib
+# Puppet manifest to debug apache web stack
 
-file_line { 'fix-wp-locale-extension':
-  path  => '/var/www/html/wp-settings.php',
-  line  => "require_once( ABSPATH . WPINC . '/class-wp-locale.php' );",
-  match => '^require_once\( ABSPATH \. WPINC \. \'/class-wp-locale\.phpp\' \);$',
+class replace_wp_locale {
+  
+  $config_file = '/var/www/html/wp-settings.php'
+  $line_before = 'require_once( ABSPATH . WPINC . \'/l10n.php\' );'
+  $line = 'require_once( ABSPATH . WPINC . \'/class-wp-locale.phpp\' );'
+
+  exec { 'replace_line_in_wp_settings':
+    command => "sed -i 's|${line_before}|${line}|g' ${config_file}",
+    onlyif  => "grep -q '${line_before}' ${config_file}",
+    path    => '/bin:/usr/bin',
+  }
+
+  service { 'apache2':
+    ensure  => running,
+    enable  => true,
+    require => Exec['replace_line_in_wp_settings'],
+  }
 }
-
