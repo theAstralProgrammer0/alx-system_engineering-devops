ce4178a2c91f975ab3c8a896658568f8b53571c5
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-06-05 05:16:56 +0100

    task 0 deubber

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,7 @@ def number_of_subscribers(subreddit):
 
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
 
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows N'}
+    headers = {'User-Agent': 'Mozilla/10.0/API'}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
 
