8a8e2813265eac90db829a7b9e758638c2c01558
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-06-14 10:31:38 +0100

    debugging

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 # puppet manifes to reduce failed requests
 
 exec { 'search_and_replace':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx/',
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
 }
 
