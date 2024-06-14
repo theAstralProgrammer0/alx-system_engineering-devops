2c37a5d3956bd710709489e54c7d81bd91cc8f2b
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-06-14 09:51:18 +0100

    restarted the nginx web server

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -6,3 +6,8 @@ exec { 'search_and_replace':
   command => "sed -i 's/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/g' ${file}",
   path    => ['/bin', '/usr/bin']
 }
+
+-> exec { 'restart_nginx':
+     command => "service nginx restart",
+     path    => ['/bin', '/usr/bin']
+  }
