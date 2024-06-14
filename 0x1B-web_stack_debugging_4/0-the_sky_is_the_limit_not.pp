6b5e65bbfd599484cc3cb43c4f2d938892ecdebb
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-06-14 10:29:25 +0100

    debugging

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,10 +2,10 @@
 
 exec { 'search_and_replace':
   command => 'sed -i "s/15/4096/" /etc/default/nginx/',
-  path    => ['/bin', '/usr/bin', '/usr/local/bin']
+  path    => '/usr/local/bin/:/bin/'
 }
 
 exec { 'restart_nginx':
   command => 'nginx restart',
-  path    => '/etc/init.d'
+  path    => '/etc/init.d/'
 }
